6a2e7a49f8b6cfb234c7c901e8f64304187b0901
ViniciusMarquesp <ciusivinicius@hotmail.com>
2021-08-18 14:51:53 -0300

    Implementações

--- FUNDAMENTOS WEB/js/script.js
+++ FUNDAMENTOS WEB/js/script.js
@@ -1,8 +1,30 @@
-const teste = 'teste';
+// selecionar o elemento no html
+let nome = window.document.getElementById("nome"); // select por id
+let email = document.querySelector("#email"); // select por qualquer #id .classe
+let assunto = document.querySelector("assunto");
 
-console.log(teste);
+nome.style.width = "100%";
+email.style.width = "100%";
 
-const elemento = document.getElementById('new');
-const text = document.createTextNode('vini otário')
-elemento.appendChild(text)
+function validaNome() {
+  let txtNome = document.querySelector("#txtNome");
+  if (nome.value.length < 3) {
+    txtNome.innerHTML = "Nome Inválido";
+    txtNome.style.color = "red";
+  } else {
+    txtNome.innerHTML = "Nome Válido";
+    txtNome.style.color = "green";
+  }
+}
 
+function validaEmail() {
+  let txtEmail = document.querySelector("#txtEmail");
+
+  if ((email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1)) {
+    txtEmail.innerHTML = "E-mail inválido";
+    txtEmail.style.color = "red";
+  } else {
+    txtEmail.innerHTML = "E-mail Válido";
+    txtEmail.style.color = "green";
+  }
+}
