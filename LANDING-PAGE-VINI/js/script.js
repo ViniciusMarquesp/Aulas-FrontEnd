b0ff73e4594c575ed774fa764855f82d69cad77b
ViniciusMarquesp <ciusivinicius@hotmail.com>
2021-08-18 17:14:25 -0300

    Atualizações

--- LANDING-PAGE-VINI/js/script.js
+++ LANDING-PAGE-VINI/js/script.js
@@ -1,26 +1,48 @@
-//Case Sensitive => reconhece letras MAIUSCULAS e minusculas(e diferencia elas)
+// Case Sensitive => reconhece letras MAIUSCULAS e minusculas (e diferencia elas)
+// thiago / Thiago
 
-//document selector => ensinando a pegar elementos
-//por tag: document.getElementsByTagName("h3")
-//por classe: document.getElementsByClassName(".nomedaclasse")
-//por nome: document.getElementsByName("email")
-//por ID: document.getElementById("#idDoElemento")
-//por seletor: document.querySelector(coringa ele pega por tudo)
+// document selector
+// por tag: document.getElementsByTagName('h3')
+// por classe: document.getElementsByClassName('.nomedaclasse')
+// por nome: document.getElementsByName('email')
+// por ID: document.getElementById('#idDoelemento')
+// por seletor: document.querySelector('id/classe/nome')
 
-//Variaveis => métodos para declarar
-//   var nome = "Vinicius"  //jeito mais antigo
-// let nome = "Vinicius"  //jeito mais atual - variavel que vai sofrer mudanças
-//   const nome = "Vinicius" //jeito mais atual - variavel 'fixa' 
+// Variaveis:
+//   var nome = 'thiago' //jeito mais antigo
+//   let nome = 'thiago' //jeito mais atual - variavel que vai sofrer mudanças
+//   const nome = 'thiago' //jeito mais atual - variavel 'fixa'
 
-let digiteAlgo = document.querySelector('#digiteAlgo')
+let digiteAlgo = document.querySelector('#digiteAlgo');
 
 function validaCampo() {
-    if(digiteAlgo.value.length < 5) {
-        digiteAlgo.style.color = 'red'
-        digiteAlgo.style.border = '1px solid red'
-    } else {
-        digiteAlgo.style.color = 'green'
-        digiteAlgo.style.border = '1px solid green'
-    }
+  if (digiteAlgo.value.length < 5 ) {
+    digiteAlgo.style.color = 'red';
+    digiteAlgo.style.border = '1px solid red'
+  } else {
+    digiteAlgo.style.color = 'green'
+    digiteAlgo.style.border = '1px solid green';
+  }
 }
+
+
+
+let modal = document.getElementById('myModal')
+let btn = document.getElementById('myBtn')
+let span = document.getElementsByClassName('close')[0]
+
+btn.onclick = function(){
+  modal.style.display = 'block';
+}
+
+span.onclick = function(){
+  modal.style.display = 'none';
+}
+
+window.onclick= function(event){
+  if (event.target == modal){
+    modal.style.display = 'none';
+  }
+}
+
 
